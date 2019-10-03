0024425edc2f507f5d2c46d679df8d2528343379
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2019-10-03 18:52:48 -0400

    Add files via upload

--- Python_label_encoder.py
+++ Python_label_encoder.py
@@ -4,7 +4,7 @@ data =
 subset = 
 picked_column = data['subset']
 
-def encode_str_to_num:
+def encode_str_to_num(data):
 	#use the LabelEncoder to make shopname numerical
 	LE = LabelEncoder()
 	data['feat_shopname'] = LE.fit_transform(data['shopname'])
