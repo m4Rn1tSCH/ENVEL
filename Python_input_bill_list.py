dd8310bd6304c24df467a29c65bc5264ce516184
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2019-10-10 21:43:32 -0400

    Add files via upload

--- Python_input_bill_list.py
+++ Python_input_bill_list.py
@@ -1,7 +1,9 @@
 #input for the bill_list to scan
 bill_list = []
 
-b = input("Enter a recurring bill:")
-bill_list = bill_list.append(b)
+print("Enter your bill or receipt manually")
+b = input()
+b_formatted = ''.format(', b, ')
+bill_list = bill_list.append(b_formatted)
 print (b , "has been added to the list")
 
