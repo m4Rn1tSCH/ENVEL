6dc4c6ed9c4b34fd8cf2e095c26940fae137e4c4
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2019-10-15 15:50:56 -0400

    Add files via upload

--- Python_random_sample.py
+++ Python_random_sample.py
@@ -1,17 +1,20 @@
 #create a random sample
-
-
+#picking 32 rows randomly (not subsequent ones) from the data and ranking it by date in ascending order (long ago to recent)
 #set number of drawn rows/columns, optionally set a weight and a reproducible pseudo-random result
 
+##data = pandas data frame
+##GIVE INTEGERS ONLY
 sample_size = 32
 sample_weights = None
 random_state = None
 
-def draw_sample:
+def draw_sample(data, sample_size, sample_weights, random_state):
 	#draw the sample and rank it
-	random_sample = data.sample(n = sample_size, frac = None, replace = False, weights = sample_weights, random_state = random_state, axis=None)
+	random_sample = data.sample(n = sample_size, frac = None, replace = False, weights = sample_weights,
+								random_state = random_state, axis=None)
 
 	#ranking with index (axis = 0)
-	ranked_sample = random_sample.rank(axis = 0, method = 'min', numeric_only = None, na_option = 'keep', ascending = True, pct = False)
+	ranked_sample = random_sample.rank(axis = 0, method = 'min', numeric_only = None, na_option = 'keep',
+										ascending = True, pct = False)
 	print(ranked_sample.head(5))
 	df.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
