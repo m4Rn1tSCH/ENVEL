ad30f61dc1603f9d715ce443c327a10535ab974c
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2019-10-16 10:26:36 -0400

    Add files via upload

--- Python_random_sample.py
+++ Python_random_sample.py
@@ -9,12 +9,12 @@ sample_weights = None
 random_state = None
 
 def draw_sample(data, sample_size, sample_weights, random_state):
-	#draw the sample and rank it
-	random_sample = data.sample(n = sample_size, frac = None, replace = False, weights = sample_weights,
-								random_state = random_state, axis=None)
+    #draw the sample and rank it
+    random_sample = data.sample(n = sample_size, frac = None, replace = False, weights = sample_weights,
+                             random_state = random_state, axis=None)
 
-	#ranking with index (axis = 0)
-	ranked_sample = random_sample.rank(axis = 0, method = 'min', numeric_only = None, na_option = 'keep',
-										ascending = True, pct = False)
-	print(ranked_sample.head(5))
-	df.sample(n=None, frac=None, replace=False, weights=None, random_state=None, axis=None)
+    #ranking with index (axis = 0)
+    ranked_sample = random_sample.rank(axis = 0, method = 'min', numeric_only = None, na_option = 'keep',
+                                    ascending = True, pct = False)
+    print(ranked_sample.head(5))
+    
