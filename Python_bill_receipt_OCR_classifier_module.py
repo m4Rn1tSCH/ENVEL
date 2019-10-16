558a42e5ce441f41243fa5a32ca8eba409f4609a
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2019-10-16 15:38:01 -0400

    Add files via upload

--- Python_bill_receipt_OCR_classifier_module.py
+++ Python_bill_receipt_OCR_classifier_module.py
@@ -4,12 +4,16 @@ import pyocr
 import pyocr.builders
 from PIL import Image
 import pdf2image
-import tempfile2 as tmp
+import tempfile as tmp
+import tempfile2 as tmp2
 #%%
 #list of possible bills
 list_bills = ['Eversource', 'NATIONALGRID', 'AMAZON', 'EVERSOURCE']
 bills_found = list()
-
+#%%
+temp_dir = tmp.gettempdir()
+temp_f = tmp2.TemporaryDirectory(suffix=None, prefix=None, dir=temp_dir)
+#%%
 #input link (in APP)
 #filename of your bill in PDF
 #directory where your PDF is stored
@@ -27,14 +31,13 @@ file_in = ''.join(('', new_link_2,''))
 pdf2image.convert_from_path(file_in, dpi=400, output_folder= 'C:/Users/bill-/Desktop/', 
                             first_page=0, last_page=5, fmt='png', 
                             thread_count=1, userpw=None, use_cropbox=False, strict=False, 
-                            transparent=False, output_file='C:/Users/bill-/Desktop/4a90245d-9f16-413f-8c8d-23469f4775db')
+                            transparent=False, output_file=temp_dir)
 
-temp_pics = tmp.SpooledTemporaryFile('C:/Users/bill-/Desktop/4a90245d-9f16-413f-8c8d-23469f4775db')
 #%%
 ####GET A VERSION WITH HASHLIB WORKING!
 #OCR process
 tools = pyocr.get_available_tools()[0]
-text = tools.image_to_string(Image.open(temp_pics), 
+text = tools.image_to_string(Image.open(temp_f), 
 					builder=pyocr.builders.DigitBuilder())
 
 strings = [text]
@@ -43,7 +46,7 @@ print(text)
 #search for string in text (Eversource; Nationalgrid etc.)
 for names in list_bills:
 
-    if text.str.contains(list_bills):
+    if strings.str.contains(list_bills):
         bills_found.append(names)
         #menu for confirmation
 #IF FAILED
@@ -52,15 +55,15 @@ for names in list_bills:
 #%%
 #conversion colored image to monochrome mode
 #filename of your JPEG/PNG
-link = r"C:\Users\bill-\Desktop\*.png" 
+link = temp_f 
 #convert backslashes to slashes
-new_link = link.replace(os.sep, '/')
+#new_link = link.replace(os.sep, '/')
 #remove those pesky letter strings
-new_link_2 = new_link.replace('"', '')
-input_pic = ''.join(('', new_link,''))
+#new_link_2 = new_link.replace('"', '')
+#input_pic = ''.join(('', new_link,''))
 
 #opens the JPEG/PNG and converts it to monochrome
-img = Image.open(input_pic)
+img = Image.open(link)
 img = img.convert('L')
 img.save('monochrome_image.jpg')
 
