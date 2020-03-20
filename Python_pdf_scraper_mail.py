9c0b8cbd2bcc08dcc995c660f94009f8ed0a8c04
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-03-20 11:32:20 -0400

    Update Python_pdf_scraper_mail.py
    python script that reads the pdfs and  writes them to a list
    PLANNED: automatically move files with an error to a separate folder

--- Python_pdf_scraper_mail.py
+++ Python_pdf_scraper_mail.py
@@ -13,23 +13,26 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 import re
 import os
+import csv
+import pandas as pd
+import shutil
 #%%
-def get_cv_email(self, cv_path):
-    pagenums = set()
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-    infile = open(cv_path, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close()
-    match = re.search(r'[\w\.-]+@[\w\.-]+', text)
-    email = match.group(0)
-    return email
+#def get_cv_email(self, cv_path):
+#    pagenums = set()
+#    output = StringIO()
+#    manager = PDFResourceManager()
+#    converter = TextConverter(manager, output, laparams=LAParams())
+#    interpreter = PDFPageInterpreter(manager, converter)
+#    infile = open(cv_path, 'rb')
+#    for page in PDFPage.get_pages(infile, pagenums):
+#        interpreter.process_page(page)
+#    infile.close()
+#    converter.close()
+#    text = output.getvalue()
+#    output.close()
+#    match = re.search(r'[\w\.-]+@[\w\.-]+', text)
+#    email = match.group(0)
+#    return email
 
 #\w+(?:[.-]\w+)*@\w+(?:[.-]\w+)+[.-][a-z_0-9]+(?=[A-Z]|(?!=[.-])\b)
 #%%
@@ -56,8 +59,61 @@ def get_cv_email(self, cv_path):
     converter.close()
     text = output.getvalue()
     output.close()
-    match = re.search(r'\w+(?:[.-]\w+)*@\w+(?:[.-]\w+)+[.-][a-z_0-9]+(?=[A-Z]|(?!=[.-])\b', text)
+    match = re.search(r'\w+(?:[.-]\w+)*@\w+(?:[.-]\w+)+[.-][a-z_0-9]+(?=[A-Z]|(?!=[.-])\b)', text)
     email = match.group(0)
     return email
 
 #\w+(?:[.-]\w+)*@\w+(?:[.-]\w+)+[.-][a-z_0-9]+(?=[A-Z]|(?!=[.-])\b)
+#%%
+#this one extracts 29 mails successfully
+#one bugged PDF
+pure_email_list = []
+error_list = []
+#source_folder = os.path.relpath(r'C:\Users\bill-\Desktop\Harvard_Resumes\*.csv')
+#destination = os.path.relpath(r'C:\Users\bill-\Desktop\Harvard_Resumes_converted')
+#destination_failed = os.path.relpath(r'C:\Users\bill-\Desktop\Harvard_Resumes_failed')
+'''
+try control has to be inside the for loop; while iterating the attempt shall be made
+and if an exception occurs, it shall be appended to the list and the loop is to be continued
+'''
+for link in path_list:
+    try:
+        pagenums = set()
+        output = StringIO()
+        manager = PDFResourceManager()
+        converter = TextConverter(manager, output, laparams=LAParams())
+        interpreter = PDFPageInterpreter(manager, converter)
+        infile = open(link, 'rb')
+        for page in PDFPage.get_pages(infile, pagenums):
+            interpreter.process_page(page)
+        infile.close()
+        converter.close()
+        text = output.getvalue()
+        output.close()
+        match = re.search(r'\w+(?:[.-]\w+)*@\w+(?:[.-]\w+)+[.-][a-z_0-9]+(?=[A-Z]|(?!=[.-])\b)', text)
+        email = match.group(0)
+        pure_email_list.append(email)
+        #shutil.move(source_folder, destination)
+        #with open('C:/Users/bill-/Desktop/Harvard_mail_list.csv','a') as newFile:
+            #newFileWriter=csv.writer(newFile)
+#if more than one element; in squared brackets
+            #newFileWriter.writerow(email)
+#should an exception be hit it will append the bugged PDF path to the error_list
+    except:
+        error_list.append(link)
+        #shutil.move(source_folder,destination_failed)
+        pass
+print("Following motherfuckers were to fucking dumb to create a properly readable PDF...")
+print("-------------------------------------------------------------------------------")
+print(error_list)
+pd.DataFrame(pure_email_list).to_csv('C:/Users/bill-/Desktop/Harvard_mail_list.csv')
+pd.DataFrame(error_list).to_csv('C:/Users/bill-/Desktop/Harvard_fail_list.csv')
+#%%
+#error_list = []
+#pure_mail_list = []
+#try:
+#    for link in path_list:
+#        link.get_cv_email()
+#        pure_mail_list.append(link)
+#except:
+#    error_list.append(link)
