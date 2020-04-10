ca57d996ae559f12651907eb429c50a4e8aec721
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-04-10 12:16:41 -0400

    EDA_basis works
    EDA ready to take a randomly picked user id and produce a dataframe

--- Python_EDA_users.py
+++ Python_EDA_users.py
@@ -21,13 +21,35 @@ import matplotlib.pyplot as plt
 #generates a CSV for daily/weekly/monthly account throughput; expenses and income
 from Python_spending_report_csv_export_function import spending_report
 #contains the connection script
-from Python_SQL_connection import *
+from Python_SQL_connection import execute_read_query, create_connection, close_connection
 #contains all credentials
 import PostgreSQL_credentials as acc
 #%%
-#estbalish
+#Py_SQL_con needs to be loaded first
+connection = create_connection(db_name = acc.YDB_name, db_user = acc.YDB_user, db_password = acc.YDB_password, db_host = acc.YDB_host, db_port = acc.YDB_port)
+#%%
+#estbalish connection to get user IDs
 filter_query = f"SELECT unique_mem_id, state, city, zip_code, income_class, file_created_date FROM user_demographic WHERE state = 'MA'"
-    transaction_query = execute_read_query(connection, filter_query)
-    query_df = pd.DataFrame(transaction_query,
+transaction_query = execute_read_query(connection, filter_query)
+query_df = pd.DataFrame(transaction_query,
                         columns = ['unique_mem_id', 'state', 'city', 'zip_code', 'income_class', 'file_created_date'])
-
+#%%
+#dateframe to gather MA bank data
+try:
+    for i in query_df['unique_mem_id'].sample(n = 1, random_state = 2):
+        print(i)
+        filter_query = f"SELECT * FROM bank_record WHERE unique_mem_id = '{i}'"
+        transaction_query = execute_read_query(connection, filter_query)
+        bank_df = pd.DataFrame(transaction_query,
+                        columns = ['unique_mem_id', 'unique_bank_account_id', 'unique_bank_account_id','amount',
+                                   'currency', 'description', 'transaction_date', 'post_date',
+                                   'transaction_base_type', 'transaction_category_name', 'primary_merchant_name',
+                                   'secondary_merchant_name', 'city','state', 'zip_code', 'transaction_origin',
+                                   'factual_category', 'factual_id', 'file_created_date', 'optimized_transaction_date',
+                                   'yodlee_transaction_status', 'mcc_raw', 'mcc_inferred', 'swipe_date',
+                                   'panel_file_created_date', 'update_type', 'is_outlier', 'change_source',
+                                   'account_type', 'account_source_type', 'account_score', 'user_score', 'lag', 'is_duplicate'])
+        print(f"User {i} has {len(bank_df)} transactions on record.")
+except OperationalError as e:
+        print(f"The error '{e}' occurred")
+        connection.rollback
