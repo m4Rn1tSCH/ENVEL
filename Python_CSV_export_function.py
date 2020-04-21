e1baa32df7126dcb495280d9617df1fad38ea25f
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-04-21 12:22:51 -0400

    refactor
    -csv no takes name argument
    -eda not genrates the user id df and the encoded one

--- Python_CSV_export_function.py
+++ Python_CSV_export_function.py
@@ -16,21 +16,21 @@ This function converts a dataframe to CSV and just work in appending mode
 if the csv file is already existing.
 Saves with a timestamp and generates in the current working directory
 '''
-def csv_export(df):
+def csv_export(df, file_name):
     #local working directory
     raw = os.getcwd()
     #folder when executed on the AWS instance
     #aws = os.mkdir('/injection')
     date_of_creation = dt.today().strftime('%m-%d-%Y_%Hh-%mmin')
 
-    csv_path = os.path.abspath(os.path.join(raw, date_of_creation + '_REPORT' + '.csv'))
+    csv_path = os.path.abspath(os.path.join(raw, date_of_creation + file_name + '_REPORT' + '.csv'))
 
     try:
         df.to_csv(csv_path)
     except FileExistsError as exc:
         print(exc)
         print("existing file will be appended instead...")
-        csv_path = os.path.abspath(os.path.join(raw, date_of_creation + '_REPORT_2' + '.csv'))
+        csv_path = os.path.abspath(os.path.join(raw, date_of_creation + file_name + '_REPORT' + '.csv'))
         df.to_csv(csv_path, mode = 'a', header = False)
 
 #close the function with return xx to avoid error 500 when querying the URL and have a message showing up instead
