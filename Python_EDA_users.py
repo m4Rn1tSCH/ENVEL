3001dbf078bdcba4ea52f470a942cd3e9ec07352
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-04-27 15:38:45 -0400

    smaller fixes
    - docs
    -pipelines

--- Python_EDA_users.py
+++ Python_EDA_users.py
@@ -852,23 +852,20 @@ print(f"Pipeline 6; {dt.today()}")
 print(grid_search.fit(X_train, y_train).best_params_)
 print(f"Best accuracy with parameters: {grid_search.best_score_}")
 #%%
-
-
+#accuracy negative; model toally off
 transformer = QuantileTransformer(output_distribution='normal')
 regressor = LinearRegression()
 regr = TransformedTargetRegressor(regressor=regressor,
-...                                   transformer=transformer)
+                                   transformer=transformer)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 regr.fit(X_train, y_train)
 
 TransformedTargetRegressor(...)
 print('R2 score: {0:.2f}'.format(regr.score(X_test, y_test)))
-R2 score: 0.67
+
 
 raw_target_regr = LinearRegression().fit(X_train, y_train)
 print('R2 score: {0:.2f}'.format(raw_target_regr.score(X_test, y_test)))
-R2 score: 0.64
 #%%
 '''
 Random Forest Classifier
