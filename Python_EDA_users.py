f716c473c42f301ed12880148f18f5dd86d5306e
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-05-06 14:07:28 -0400

    rename file
    -renamed a file
    -pop another column to see if it fixes the problem

--- Python_EDA_users.py
+++ Python_EDA_users.py
@@ -1371,17 +1371,15 @@ feature_columns_container.append(city_ind)
 ##########################
 #%%
 # A utility method to create a tf.data dataset from a Pandas Dataframe
-dataframe=bank_df
-def df_to_dataset(dataframe, shuffle = True, batch_size = len(dataframe.index/2)):
+def df_to_dataset(dataframe, shuffle = True, batch_size = len(dataframe.index/3)):
     dataframe = dataframe.copy()
-    labels = dataframe.pop('city')
+    labels = dataframe.pop('account_score')
     ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
     if shuffle:
         ds = ds.shuffle(buffer_size = len(dataframe))
         ds = ds.batch(batch_size)
 
     return ds
-
 #%%
 ##STEP 2
 #create layers
