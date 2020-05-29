f3f42d36f5fbf2a9f24f578365133c8dc8b2e20a
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-05-29 11:48:58 -0400

    multivariate regression added
    -bug in loss function

--- Python_TF2_NN_regression.py
+++ Python_TF2_NN_regression.py
@@ -631,7 +631,7 @@ model.add(layers.SimpleRNN(128))
 
 model.add(layers.Dense(10))
 
-model.summary() 
+model.summary()
 
 encoder_vocab = 1000
 decoder_vocab = 2000
@@ -710,7 +710,7 @@ def build_model(allow_cudnn_kernel=True):
         lstm_layer = tf.keras.layers.RNN(
             tf.keras.layers.LSTMCell(units),
             input_shape=(None, input_dim))
-    
+
     model = tf.keras.models.Sequential([
         lstm_layer,
         tf.keras.layers.BatchNormalization(),
@@ -720,7 +720,7 @@ def build_model(allow_cudnn_kernel=True):
 #%%
 model = build_model(allow_cudnn_kernel=False)
 
-model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               optimizer='sgd',
               metrics=['accuracy'])
 
@@ -728,3 +728,110 @@ model.fit(dataset,
           batch_size=batch_size,
           epochs=5,
           verbose=2)
+#%%
+##################################################
+'''
+            Multivariate Regression
+'''
+# picking features
+features = bank_df
+target = bank_df.copy().pop('amount_mean_lag7')
+features.index = bank_df.index
+features.head()
+features[['amount', 'amount_std_lag7']].plot(subplots=True)
+# split and normalize data
+dataset = features.values
+TRAIN_SPLIT = 250
+# normalize
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+dataset = (dataset-data_mean)/data_std
+
+#%%
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                      target_size, step, single_step=False):
+  data = []
+  labels = []
+
+  start_index = start_index + history_size
+  if end_index is None:
+    end_index = len(dataset) - target_size
+
+  for i in range(start_index, end_index):
+    indices = range(i-history_size, i, step)
+    data.append(dataset[indices])
+
+    if single_step:
+      labels.append(target[i+target_size])
+    else:
+      labels.append(target[i:i+target_size])
+
+  return np.array(data), np.array(labels)
+#%%
+
+past_history = 100
+future_target = 50
+STEP = 6
+BATCH_SIZE = 150
+BUFFER_SIZE = 256
+# whole dataset is taken as features; amont_mean_lag7 as target
+X_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
+                                                   TRAIN_SPLIT, past_history,
+                                                   future_target, STEP,
+                                                   single_step=True)
+X_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
+                                               TRAIN_SPLIT, None, past_history,
+                                               future_target, STEP,
+                                               single_step=True)
+
+# tensor created from separate variablesp assed as tuple
+train_data_single = tf.data.Dataset.from_tensor_slices((X_train_single, y_train_single))
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_single = tf.data.Dataset.from_tensor_slices((X_val_single, y_val_single))
+val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+
+single_step_model = tf.keras.models.Sequential()
+single_step_model.add(tf.keras.layers.LSTM(32,
+                                           input_shape=X_train_single.shape[-2:]))
+single_step_model.add(tf.keras.layers.Dense(1))
+
+single_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(0.001),
+                          loss='mse')
+
+
+for x, y in val_data_single.take(1):
+  print(single_step_model.predict(x).shape)
+
+EPOCHS = 150
+EVALUATION_INTERVAL = 200
+
+single_step_history = single_step_model.fit(train_data_single, epochs=EPOCHS,
+                                            steps_per_epoch=EVALUATION_INTERVAL,
+                                            validation_data=val_data_single,
+                                            validation_steps=50)
+
+def plot_train_history(history, title):
+    loss = history.history[['mae', 'mse']]
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+
+    plt.show()
+
+plot_train_history(single_step_history,
+                   'Single Step Training and validation loss')
+
+for x, y in val_data_single.take(3):
+  plot = show_plot([x[0][:, 1].numpy(), y[0].numpy(),
+                    single_step_model.predict(x)[0]], 12,
+                   'Single Step Prediction')
+  plot.show()
