7c90b4913df0e228f58408b9d34357c7eed678ce
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-06-01 15:09:36 -0400

    prep for Flask

--- Python_TF2_NN_regression.py
+++ Python_TF2_NN_regression.py
@@ -36,7 +36,8 @@ import PostgreSQL_credentials as acc
 #csv export with optional append-mode
 from Python_CSV_export_function import csv_export
 #%%
-
+app = Flask(__name__)
+@app.route('/df_encoder')
 def df_encoder(rng = 4):
 
     '''
@@ -367,7 +368,7 @@ def df_encoder(rng = 4):
 
     df.set_index(date_index, drop = False, inplace=True)
     df = df.dropna()
-    #drop user IDs to avoid overfitting with useless information
+    # drop user IDs to avoid overfitting with useless information
     df = df.drop(['unique_mem_id',
                             'unique_bank_account_id',
                             'unique_bank_transaction_id'], axis = 1)
@@ -384,87 +385,81 @@ def df_encoder(rng = 4):
 '''
                 Tensorflow Regression
 '''
-app = Flask(__name__)
-
-@app.route('/tf_regression')
-def tf_regression():
-
-    print("tensorflow regression running...")
-    bank_df = df_encoder(rng=4)
-    column_names = [bank_df.columns.values]
-    dataset = bank_df.copy()
-    print(dataset.head())
-    print(dataset.tail())
-    print(dataset.isna().sum())
-    sns.pairplot(bank_df[['amount', 'amount_mean_lag7', 'amount_std_lag7']])
-    #%%
-    # setting label and features (the df itself here)
-    model_label = dataset.pop('amount_mean_lag7')
-    model_label.astype('int64')
-
-    # EAGER EXECUTION NEEDS TO BE ENABLED HERE
-    # features and model labels passed as tuple
-    tensor_ds = tf.data.Dataset.from_tensor_slices((dataset.values, model_label.values))
-    for feat, targ in tensor_ds.take(5):
-        print('Features: {}, Target: {}'.format(feat, targ))
-
-    train_dataset = tensor_ds.shuffle(len(bank_df)).batch(2)
-    #%%
-    #OPTIONAL WAY TO FEED DATA
-    ##########################################
-    # ALTERNATIVE TO FEED FEATURES TO THE MODEL AS DICTIONARY CONSISITING OF DF KEYS
-    # inputs = {key: tf.keras.layers.Input(shape=(), name=key) for key in df.keys()}
-    # x = tf.stack(list(inputs.values()), axis=-1)
-
-    # x = tf.keras.layers.Dense(10, activation='relu')(x)
-    # output = tf.keras.layers.Dense(1)(x)
-
-    # model_func = tf.keras.Model(inputs=inputs, outputs=output)
-
-    # model_func.compile(optimizer='adam',
-    #                     loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-    #                     metrics=['mse', 'mae'])
-
-    # dict_slices = tf.data.Dataset.from_tensor_slices((df.to_dict('list'), target.values)).batch(16)
-    # for dict_slice in dict_slices.take(1):
-    #   print (dict_slice)
-    # model_func.fit(dict_slices, epochs=15)
-    ###########################################
-    #%%
-    # if numpy arrays are given the first layers needs to be layers.Flatten and specify the quadratic input shape
-    # REGULARIZATION: add dropout or regularizers in each layer
-    # combined reg + dropout produces best results
-    # Dropout layers sets variables to zero in randomized patterns
-    # when adding weight relugarizer - monitor bin cross entropy directly
-    def compile_model():
-        model = tf.keras.Sequential([
-        # initial layer is input layers
-        tf.keras.layers.Dense(32, activation='relu',
-                              kernel_regularizer=regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(32, activation='relu',
-                              kernel_regularizer=regularizers.l2(0.001)),
-        tf.keras.layers.Dropout(0.2),
-        # final layers is output layers
-        tf.keras.layers.Dense(1)
-        ])
-        # gradients / running average; optimizes stochastic gradient descent
-        optimizer = tf.keras.optimizers.RMSprop(0.001)
-        model.compile(optimizer=optimizer,
-                      loss='mse',
-                      metrics=['mse', 'mae'])
-        return model
-    #%%
-    model = compile_model()
-    #model.fit(train_dataset, epochs=15)
-
-    EPOCHS = 50
-    # when dataset given; only ds needed (no y needed)
-    # when separate tensors given; arg x and y needed
-    history = model.fit(train_dataset, epochs=EPOCHS, verbose=2)
-
-    # regularizer_histories['reg_and_dropout'] = compile_model(combined_model, "regularizers/combined")
-    # plt.plot(history)
 
 
+print("tensorflow regression running...")
+bank_df = df_encoder(rng=4)
+dataset = bank_df.copy()
+print(dataset.head())
+print(dataset.tail())
+print(dataset.isna().sum())
+sns.pairplot(bank_df[['amount', 'amount_mean_lag7', 'amount_std_lag7']])
+#%%
+# setting label and features (the df itself here)
+model_label = dataset.pop('amount_mean_lag7')
+model_label.astype('int64')
+
+# EAGER EXECUTION NEEDS TO BE ENABLED HERE
+# features and model labels passed as tuple
+tensor_ds = tf.data.Dataset.from_tensor_slices((dataset.values, model_label.values))
+for feat, targ in tensor_ds.take(5):
+    print('Features: {}, Target: {}'.format(feat, targ))
+
+train_dataset = tensor_ds.shuffle(len(bank_df)).batch(2)
+#%%
+#OPTIONAL WAY TO FEED DATA
+##########################################
+# ALTERNATIVE TO FEED FEATURES TO THE MODEL AS DICTIONARY CONSISTING OF DF KEYS
+# inputs = {key: tf.keras.layers.Input(shape=(), name=key) for key in df.keys()}
+# x = tf.stack(list(inputs.values()), axis=-1)
+
+# x = tf.keras.layers.Dense(10, activation='relu')(x)
+# output = tf.keras.layers.Dense(1)(x)
+
+# model_func = tf.keras.Model(inputs=inputs, outputs=output)
+
+# model_func.compile(optimizer='adam',
+#                     loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+#                     metrics=['mse', 'mae'])
+
+# dict_slices = tf.data.Dataset.from_tensor_slices((df.to_dict('list'), target.values)).batch(16)
+# for dict_slice in dict_slices.take(1):
+#   print (dict_slice)
+# model_func.fit(dict_slices, epochs=15)
+###########################################
+#%%
+# if numpy arrays are given the first layers needs to be layers.Flatten and specify the quadratic input shape
+# REGULARIZATION: add dropout or regularizers in each layer
+# combined reg + dropout produces best results
+# Dropout layers sets variables to zero in randomized patterns
+# when adding weight relugarizer - monitor bin cross entropy directly
+def compile_model():
+    model = tf.keras.Sequential([
+    # initial layer is input layers
+    tf.keras.layers.Dense(32, activation='relu',
+                          kernel_regularizer=regularizers.l2(0.001)),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(32, activation='relu',
+                          kernel_regularizer=regularizers.l2(0.001)),
+    tf.keras.layers.Dropout(0.2),
+    # final layers is output layers
+    tf.keras.layers.Dense(1)
+    ])
+    # gradients / running average; optimizes stochastic gradient descent
+    optimizer = tf.keras.optimizers.RMSprop(0.001)
+    model.compile(optimizer=optimizer,
+                  loss='mse',
+                  metrics=['mse', 'mae'])
+    return model
+#%%
+model = compile_model()
+#model.fit(train_dataset, epochs=15)
+
+EPOCHS = 50
+# when dataset given; only ds needed (no y needed)
+# when separate tensors given; arg x and y needed
+history = model.fit(train_dataset, epochs=EPOCHS, verbose=2)
+
+# regularizer_histories['reg_and_dropout'] = compile_model(combined_model, "regularizers/combined")
+# plt.plot(history)
 
