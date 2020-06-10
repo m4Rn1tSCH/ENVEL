92aa37b47d820e82074d8e4dbf44e858e86ff5ff
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-06-10 12:01:18 -0400

    multi step fix
    -data is now handled as array and works better

--- Python_TF_LSTM_multivariate_regression.py
+++ Python_TF_LSTM_multivariate_regression.py
@@ -371,60 +371,64 @@ print("Tensorflow regression:")
 print("TF-version:", tf.__version__)
 bank_df = df_encoder(rng=4)
 dataset = bank_df.copy()
-print(dataset.head())
-sns.pairplot(bank_df[['amount', 'amount_mean_lag7', 'amount_std_lag7']])
+print(dataset.head(3))
+
+
 # split and normalize data
-dataset = features.values
 TRAIN_SPLIT = 250
-# normalize
 data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
 data_std = dataset[:TRAIN_SPLIT].std(axis=0)
-dataset = (dataset-data_mean)/data_std
+dataset_norm = (dataset-data_mean)/data_std
 #%%
 def multivariate_data(dataset, target, start_index, end_index, history_size,
                       target_size, step, single_step=False):
-  data = []
-  labels = []
+    data = []
+    labels = []
 
-  start_index = start_index + history_size
-  if end_index is None:
-    end_index = len(dataset) - target_size
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
 
-  for i in range(start_index, end_index):
-    indices = range(i-history_size, i, step)
-    data.append(dataset[indices])
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i, step)
+        data.append(dataset[indices])
 
     if single_step:
-      labels.append(target[i+target_size])
+        labels.append(target[i+target_size])
     else:
-      labels.append(target[i:i+target_size])
+        labels.append(target[i:i+target_size])
 
-  return np.array(data), np.array(labels)
+    return np.array(data), np.array(labels)
 #%%
+# shuffle data and create batches to be fed per intervall
+# DF NEEDS TO BE ARRAY HERE
+# slicing df .iloc[:, 17]
+# slicing array [:, 17]
 
-past_history = 100
-future_target = 50
-STEP = 6
-BATCH_SIZE = 150
-BUFFER_SIZE = 256
-# whole dataset is taken as features; amont_mean_lag7 as target
-X_train_single, y_train_single = multivariate_data(dataset, dataset[:, 1], 0,
-                                                   TRAIN_SPLIT, past_history,
-                                                   future_target, STEP,
+BUFFER_SIZE = len(dataset)
+past_history = 42
+future_target = 7
+STEP = 1
+
+X_train_single, y_train_single = multivariate_data(np.array(dataset), np.array(dataset)[:, 17],
+                                                   0, TRAIN_SPLIT,
+                                                   past_history, future_target,
+                                                   STEP,
                                                    single_step=True)
-X_val_single, y_val_single = multivariate_data(dataset, dataset[:, 1],
-                                               TRAIN_SPLIT, None, past_history,
-                                               future_target, STEP,
+X_val_single, y_val_single = multivariate_data(np.array(dataset), np.array(dataset)[:, 17],
+                                               TRAIN_SPLIT, None,
+                                               past_history, future_target,
+                                               STEP,
                                                single_step=True)
+
 
 # tensor created from separate variablesp assed as tuple
 train_data_single = tf.data.Dataset.from_tensor_slices((X_train_single, y_train_single))
-train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).repeat()
 
 val_data_single = tf.data.Dataset.from_tensor_slices((X_val_single, y_val_single))
-val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
-
-
+val_data_single = val_data_single.batch(BATCH_SIZE, drop_remainder=True).repeat()
+#%%
 single_step_model = tf.keras.models.Sequential()
 single_step_model.add(tf.keras.layers.LSTM(32,
                                            input_shape=X_train_single.shape[-2:]))
