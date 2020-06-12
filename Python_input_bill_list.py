e4be2019d48799b4baa99188b3a609f5e9ad359f
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-06-12 09:10:42 -0400

    clean up
    -deleted old diles or redundant ones
    -most of them still available in raw files section

--- Python_input_bill_list.py
+++
@@ -1,8 +0,0 @@
-##INPUT: STRING + EMPTY PYTHON LIST
-##OUTPUT: LIST FOR BILL RECOGNITION
-
-#input for the bill_list to scan
-bill_list = list()
-b = input("Enter your bill or receipt manually :")
-bill_list.append(b)
-print (b , "has been added to the list")
--- Python_random_sampling_ranking.py
+++
@@ -1,9 +0,0 @@
-#set number of drawn rows/columns, optionally set a weight and a reproducible pseudo-random result
-sample_size = 32
-sample_weights = None
-random_state = None
-#draw the sample and rank it
-random = data.sample(n = sample_size, frac = None, replace = False, weights = sample_weights, random_state = random_state, axis=None)
-#ranking with index (axis = 0)
-ranked = random.rank(axis = 0, method = 'min', numeric_only = None, na_option = 'keep', ascending = True, pct = False)
-print(ranked)
