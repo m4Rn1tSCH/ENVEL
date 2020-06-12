56ef55bbb443f6cba89f7e5c1b22ea24448b7bda
m4Rn1tSCH <44327801+m4Rn1tSCH@users.noreply.github.com>
2020-06-12 14:01:51 -0400

    some fixes

--- Python_TF_LSTM_multivariate_regression.py
+++ Python_TF_LSTM_multivariate_regression.py
@@ -444,26 +444,28 @@ val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
 '''
 encoder_vocab = 1000
 decoder_vocab = 2000
+output_size = 1
 
 encoder_input = layers.Input(shape=(None, ))
 encoder_embedded = layers.Embedding(input_dim=encoder_vocab, output_dim=64)(encoder_input)
 
 # Return states in addition to output
-output, state_h, state_c = layers.LSTM(
-    64, return_state=True, name='encoder')(encoder_embedded)
+output, state_h, state_c = layers.LSTM(64,
+                                       return_state=True,
+                                       name='encoder')(encoder_embedded)
 encoder_state = [state_h, state_c]
 
 decoder_input = layers.Input(shape=(None, ))
 decoder_embedded = layers.Embedding(input_dim=decoder_vocab, output_dim=64)(decoder_input)
 
 # Pass the 2 states to a new LSTM layer, as initial state
-decoder_output = layers.LSTM(
-    64, name='decoder')(decoder_embedded, initial_state=encoder_state)
-output = layers.Dense(10)(decoder_output)
+decoder_output = layers.LSTM(64,
+                             name='decoder')(decoder_embedded, initial_state=encoder_state)
+output = layers.Dense(output_size)(decoder_output)
 
 model = tf.keras.Model([encoder_input, decoder_input], output)
 model.summary()
-
+#%%
 model.compile(loss='mse',
               optimizer=tf.keras.optimizers.RMSprop(0.001),
               metrics=['mae'])
