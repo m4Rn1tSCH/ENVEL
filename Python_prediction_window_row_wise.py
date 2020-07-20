e7f7ff81a6a8537a2b94af89cf64f950cd490e56
Billateral <bill@envel.ai>
2020-07-20 14:41:11 -0400

    row_wise solved
    used generator statement

--- Python_prediction_window_row_wise.py
+++ Python_prediction_window_row_wise.py
@@ -20,12 +20,29 @@ from Python_open_pickle import open_pickle
 # dropped amount_mean_lag7 to avoid errors
 feat_merch = ['description', 'transaction_category_name', 'amount', 'state',
               'city', 'transaction_base_type', 'transaction_origin']
-
+# pull data and encode
 df = df_encoder(rng=14,
                 spending_report=False,
                 plots=False,
                 include_lag_features=False)
+# just pull raw data
+df_enc = pull_df(rng=14,
+                 spending_report=False,
+                 plots=False)
 
+# create the dict for encoded feature
+encoding_features = ['primary_merchant_name']
+UNKNOWN_TOKEN = '<unknown>'
+embedding_maps = {}
+for feature in encoding_features:
+    unique_list = df_enc[feature].unique().astype('str').tolist()
+    unique_list.append(UNKNOWN_TOKEN)
+    le = LabelEncoder()
+    le.fit_transform(unique_list)
+    embedding_maps[feature] = dict(zip(le.classes_, le.transform(le.classes_)))
+
+for key, value in embedding_maps.items():
+    print(key, value)
 
 X_train, X_train_scaled, X_train_minmax, X_test, X_test_scaled, \
     X_test_minmax, y_train, y_test = split_data(df= df,
@@ -47,6 +64,11 @@ xgb_clf_object = pipeline_xgb(x=X_array,
 
 #df[trained_model.get_booster().feature_names]
 
+# generator statement; generated once and exhausted
+dec = [embedding_maps[k] for k in sorted(embedding_maps.keys()) if k in y_pred]
+merch_val = [v for v in embedding_maps.values()]
+
+test = {key:value for key, value in embedding_maps.items()}
 # iterate through rows; apply the xgb model to each set of feat
 # df needs to boast the same columns as the training data
 for index, row in df[feat_merch].iterrows():
@@ -56,7 +78,7 @@ for index, row in df[feat_merch].iterrows():
     # y_pred = trained_model.predict(row)
 
     # insert query into dataframe (PROBLEM FOR-LOOP in SQL)
-    print(y_pred)
+    print("Index: ", index, "Prediction: ", y_pred, "", dec)
 #%%
 '''
 Catching the predictions and converting them back to merchants
@@ -64,15 +86,18 @@ Should the prediction turn out to be wrong ask for input by the user
 Label needs to be primary_merchant_name
 '''
 def merch_pred():
-    merch_list = []
-    for merchant, value in embedding_map_merchants.items():
-        for prediction in grid_search.predict(X_test):
-            if prediction == value:
-                #print(f"Transaction at {merchant}")
-                merch_list.append(merchant)
-            # else:
-            #     print("This merchant could not be recognized by us.\nCan you tell us where you are shopping right now? :)")
-            #     merch_list.append("Wrong prediction")
+
+
+    dec = [embedding_maps[k] for k in sorted(embedding_maps.keys())]
+    merch_val = [v for v in embedding_maps.values()]
+
+    test = {key:value for key, value in embedding_maps.items()}
+merch_list = []
+for i in dec:
+    if y_pred == value:
+        #print(f"Transaction at {merchant}")
+            merch_list.append(merchant)
+
     return merch_list
 
 #%%
@@ -85,7 +110,7 @@ trained_model = open_pickle(model_file="gridsearch_model.sav")
 # BUGGED
 # generate dict with merchants
 def feature_dict(feature):
-    
+
     data = df_encoder(rng=14,
                 spending_report=False,
                 plots=False,
@@ -96,7 +121,7 @@ def feature_dict(feature):
     UNKNOWN_TOKEN = "<unknown>"
     unique_list.append(UNKNOWN_TOKEN)
     LabelEncoder().fit_transform(unique_list)
-    
+
     # dict with original unique permutations as keys and transformed as values
     val_dict = {}
     val_dict = dict(zip(LabelEncoder().fit_transform(unique_list), LabelEncoder().inverse_transform())
