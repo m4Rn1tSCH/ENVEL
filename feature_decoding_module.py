c445e9a56da275dd5b1b2d6ab5e5fe4b479df004
Bill_Jaenke <bjaenke2017@student.hult.edu>
2020-09-09 17:22:53 -0400

    upd to storage

--- feature_decoding_module.py
+++ feature_decoding_module.py
@@ -34,7 +34,7 @@ def adapt_numpy_float64(numpy_float64):
 
 def adapt_numpy_int64(numpy_int64):
     return AsIs(numpy_int64)
-
+    
 def adapt_numpy_float32(numpy_float32):
     return AsIs(numpy_float32)
 
@@ -50,9 +50,133 @@ register_adapter(np.float32, adapt_numpy_float32)
 register_adapter(np.int32, adapt_numpy_int32)
 register_adapter(np.ndarray, adapt_numpy_array)
 
+def encoding_step(df, spending_report=False, include_lag_features=False):
+
+    fields = ['unique_mem_id', 'optimized_transaction_date', 'amount', 'description',
+              'primary_merchant_name', 'transaction_category_name', 'state',
+              'city', 'transaction_base_type', 'transaction_origin']    
+
+    try:
+        df['transaction_date'] = pd.to_datetime(df['transaction_date'])
+        df['optimized_transaction_date'] = pd.to_datetime(
+            df['optimized_transaction_date'])
+    except:
+        print("date columns passed. Skipping conversion.")
+        pass
+    # set optimized transaction_date as index for later
+    df.set_index('optimized_transaction_date', drop=False, inplace=True)
+
+    # generate the spending report with weekly/monthly expenses and income
+    if spending_report:
+      create_spending_report(df=df.copy())
+
+    try:
+        # Include below if needed unique ID's later:
+        # df['unique_mem_id'] = df['unique_mem_id'].astype(
+        #     'str', errors='ignore')
+        df['amount'] = df['amount'].astype('float64')
+        df['transaction_base_type'] = df['transaction_base_type'].replace(
+            to_replace=["debit", "credit"], value=[1, 0])
+    except (TypeError, OSError, ValueError) as e:
+        print(f"Problem with conversion: {e}")
+
+    date_features = (i for i in df.columns if type(i) == 'datetime64[ns]')
+    try:
+        for feature in date_features:
+            if df[feature].isnull().sum() == 0:
+                df[feature] = df[feature].apply(lambda x: dt.timestamp(x))
+            else:
+                df = df.drop(columns=feature, axis=1)
+                print(f"Column {feature} dropped")
+
+    except (TypeError, OSError, ValueError) as e:
+        print(f"Problem with conversion: {e}")
+
+    # dropping currency if there is only one
+    # try:
+    #     if len(df['currency'].value_counts()) == 1:
+    #         df = df.drop(columns=['currency'], axis=1)
+    # except:
+    #     print("Column currency was not chosen; was dropped")
+    #     pass
+
+    target_label = 'primary_merchant_name'
+    UNKNOWN_TOKEN = '<unknown>'
+    label_map = {}
+    unique_list = df[target_label].unique().astype('str').tolist()
+    unique_list.append(UNKNOWN_TOKEN)
+    le_t = LabelEncoder()
+    le_t.fit(unique_list)
+    label_map = dict(zip(le_t.classes_, le_t.transform(le_t.classes_)))
+
+    encoding_features = fields
+    UNKNOWN_TOKEN = '<unknown>'
+    embedding_map = {}
+    for feature in encoding_features:
+        unique_list = df[feature].unique().astype('str').tolist()
+        unique_list.append(UNKNOWN_TOKEN)
+        le = LabelEncoder()
+        le.fit_transform(unique_list)
+        embedding_map[feature] = dict(zip(le.classes_, le.transform(le.classes_)))
+
+        # APPLICATION TO DATASET
+        df[feature] = df[feature].apply(lambda x: x if x in embedding_map[feature] else UNKNOWN_TOKEN)
+        df[feature] = df[feature].map(lambda x: le.transform([x])[0] if type(x) == str else x)
 
 
-def db_filler(section, plots=False, spending_report=False, include_lag_features=False, store_model=False):
+    '''
+    IMPORTANT
+    The lagging features produce NaN for the first two rows due to unavailability
+    of values
+    NaNs need to be dropped to make scaling and selection of features working
+    '''
+    if include_lag_features:
+
+        date_index = df.index.values
+        df.reset_index(drop=True, inplace=True)
+        #pick lag features to iterate through and calculate features
+        lag_features = ["amount"]
+        #set up time frames; how many days/months back/forth
+        t1 = 3
+        t2 = 7
+        t3 = 30
+        #rolling values for all columns ready to be processed
+        df_rolled_3d = df[lag_features].rolling(window=t1, min_periods=0)
+        df_rolled_7d = df[lag_features].rolling(window=t2, min_periods=0)
+        df_rolled_30d = df[lag_features].rolling(window=t3, min_periods=0)
+
+        #calculate the mean with a shifting time window
+        df_mean_3d = df_rolled_3d.mean().shift(periods=1).reset_index().astype(np.float32)
+        df_mean_7d = df_rolled_7d.mean().shift(periods=1).reset_index().astype(np.float32)
+        df_mean_30d = df_rolled_30d.mean().shift(periods=1).reset_index().astype(np.float32)
+
+        #calculate the std dev with a shifting time window
+        df_std_3d = df_rolled_3d.std().shift(periods=1).reset_index().astype(np.float32)
+        df_std_7d = df_rolled_7d.std().shift(periods=1).reset_index().astype(np.float32)
+        df_std_30d = df_rolled_30d.std().shift(periods=1).reset_index().astype(np.float32)
+
+        for feature in lag_features:
+            df[f"{feature}_mean_lag{t1}"] = df_mean_3d[feature]
+            df[f"{feature}_mean_lag{t2}"] = df_mean_7d[feature]
+            df[f"{feature}_mean_lag{t3}"] = df_mean_30d[feature]
+            df[f"{feature}_std_lag{t1}"] = df_std_3d[feature]
+            df[f"{feature}_std_lag{t2}"] = df_std_7d[feature]
+            df[f"{feature}_std_lag{t3}"] = df_std_30d[feature]
+
+        df.set_index(date_index, drop=False, inplace=True)
+
+    #drop all features left with empty (NaN) values
+    df = df.dropna()
+    #drop user IDs to avoid overfitting with useless information
+    # try:
+    #     df = df.drop(['unique_mem_id'], axis=1)
+    # except:
+    #     print("Unique Member ID could not be dropped.")
+    return [df, embedding_map, label_map]
+
+
+
+def db_filler(section, store_model=False):
 
 
     # dropped amount_mean_lag7 to avoid errors
@@ -82,7 +206,7 @@ def db_filler(section, plots=False, spending_report=False, include_lag_features=
     #                        (SELECT unique_mem_id FROM user_demographic \
     #                         ORDER BY unique_mem_id ASC limit 10000 offset {10000*(section-1)}))"
     #     transaction_query = execute_read_query(connection, filter_query)
-    #     main_df = pd.DataFrame(transaction_query, columns=fields)
+    #     df = pd.DataFrame(transaction_query, columns=fields)
     #     print(f"{len(transaction_query)} transactions.")
     # except OperationalError as e:
     #     print(f"The error '{e}' occurred")
@@ -96,132 +220,18 @@ def db_filler(section, plots=False, spending_report=False, include_lag_features=
     # test_df_2 = pull_df(rng=22,
     #                   spending_report=False,
     #                   plots=False)
-    # main_df = pd.concat([test_df_1, test_df_2], axis=0)
-    main_df = pull_df(rng=22,
-                      spending_report=False,
-                      plots=False)
+    # df = pd.concat([test_df_1, test_df_2], axis=0)
+    df = pull_df(rng=22,
+                 spending_report=False,
+                 plots=False)
     ########################################
 
-    for num, user in enumerate(main_df.groupby('unique_mem_id')):
+    for num, user in enumerate(df.groupby('unique_mem_id')):
         print(f"User: {user[0]}, {num+1}/10000 users, {round(((num+1)/10000)*100, 2)}%.")
 
-
-        try:
-            main_df['transaction_date'] = pd.to_datetime(main_df['transaction_date'])
-            main_df['optimized_transaction_date'] = pd.to_datetime(
-                main_df['optimized_transaction_date'])
-        except:
-            print("date columns passed. Skipping conversion.")
-            pass
-        # set optimized transaction_date as index for later
-        main_df.set_index('optimized_transaction_date', drop=False, inplace=True)
-
-        # generate the spending report with weekly/monthly expenses and income
-        if spending_report:
-          create_spending_report(df=main_df.copy())
-
-        try:
-            # Include below if needed unique ID's later:
-            # main_df['unique_mem_id'] = main_df['unique_mem_id'].astype(
-            #     'str', errors='ignore')
-            main_df['amount'] = main_df['amount'].astype('float64')
-            main_df['transaction_base_type'] = main_df['transaction_base_type'].replace(
-                to_replace=["debit", "credit"], value=[1, 0])
-        except (TypeError, OSError, ValueError) as e:
-            print(f"Problem with conversion: {e}")
-
-        date_features = (i for i in main_df.columns if type(i) == 'datetime64[ns]')
-        try:
-            for feature in date_features:
-                if main_df[feature].isnull().sum() == 0:
-                    main_df[feature] = main_df[feature].apply(lambda x: dt.timestamp(x))
-                else:
-                    main_df = main_df.drop(columns=feature, axis=1)
-                    print(f"Column {feature} dropped")
-
-        except (TypeError, OSError, ValueError) as e:
-            print(f"Problem with conversion: {e}")
-
-        # dropping currency if there is only one
-        # try:
-        #     if len(main_df['currency'].value_counts()) == 1:
-        #         main_df = main_df.drop(columns=['currency'], axis=1)
-        # except:
-        #     print("Column currency was not chosen; was dropped")
-        #     pass
-
-        target_label = 'primary_merchant_name'
-        UNKNOWN_TOKEN = '<unknown>'
-        label_map = {}
-        unique_list = main_df[target_label].unique().astype('str').tolist()
-        unique_list.append(UNKNOWN_TOKEN)
-        le_t = LabelEncoder()
-        le_t.fit(unique_list)
-        label_map = dict(zip(le_t.classes_, le_t.transform(le_t.classes_)))
-
-        encoding_features = fields
-        UNKNOWN_TOKEN = '<unknown>'
-        embedding_map = {}
-        for feature in encoding_features:
-            unique_list = main_df[feature].unique().astype('str').tolist()
-            unique_list.append(UNKNOWN_TOKEN)
-            le = LabelEncoder()
-            le.fit_transform(unique_list)
-            embedding_map[feature] = dict(zip(le.classes_, le.transform(le.classes_)))
-
-            # APPLICATION TO DATASET
-            main_df[feature] = main_df[feature].apply(lambda x: x if x in embedding_map[feature] else UNKNOWN_TOKEN)
-            main_df[feature] = main_df[feature].map(lambda x: le.transform([x])[0] if type(x) == str else x)
-        yield embedding_map
-
-        '''
-        IMPORTANT
-        The lagging features produce NaN for the first two rows due to unavailability
-        of values
-        NaNs need to be dropped to make scaling and selection of features working
-        '''
-        if include_lag_features:
-
-            date_index = main_df.index.values
-            main_df.reset_index(drop=True, inplace=True)
-            #pick lag features to iterate through and calculate features
-            lag_features = ["amount"]
-            #set up time frames; how many days/months back/forth
-            t1 = 3
-            t2 = 7
-            t3 = 30
-            #rolling values for all columns ready to be processed
-            main_df_rolled_3d = main_df[lag_features].rolling(window=t1, min_periods=0)
-            main_df_rolled_7d = main_df[lag_features].rolling(window=t2, min_periods=0)
-            main_df_rolled_30d = main_df[lag_features].rolling(window=t3, min_periods=0)
-
-            #calculate the mean with a shifting time window
-            main_df_mean_3d = main_df_rolled_3d.mean().shift(periods=1).reset_index().astype(np.float32)
-            main_df_mean_7d = main_df_rolled_7d.mean().shift(periods=1).reset_index().astype(np.float32)
-            main_df_mean_30d = main_df_rolled_30d.mean().shift(periods=1).reset_index().astype(np.float32)
-
-            #calculate the std dev with a shifting time window
-            main_df_std_3d = main_df_rolled_3d.std().shift(periods=1).reset_index().astype(np.float32)
-            main_df_std_7d = main_df_rolled_7d.std().shift(periods=1).reset_index().astype(np.float32)
-            main_df_std_30d = main_df_rolled_30d.std().shift(periods=1).reset_index().astype(np.float32)
-
-            for feature in lag_features:
-                main_df[f"{feature}_mean_lag{t1}"] = main_df_mean_3d[feature]
-                main_df[f"{feature}_mean_lag{t2}"] = main_df_mean_7d[feature]
-                main_df[f"{feature}_mean_lag{t3}"] = main_df_mean_30d[feature]
-                main_df[f"{feature}_std_lag{t1}"] = main_df_std_3d[feature]
-                main_df[f"{feature}_std_lag{t2}"] = main_df_std_7d[feature]
-                main_df[f"{feature}_std_lag{t3}"] = main_df_std_30d[feature]
-
-            main_df.set_index(date_index, drop=False, inplace=True)
-
-        #drop all features left with empty (NaN) values
-        main_df = main_df.dropna()
-        #drop user IDs to avoid overfitting with useless information
-        # try:
-        #     main_df = main_df.drop(['unique_mem_id'], axis=1)
-        # except:
-        #     print("Unique Member ID could not be dropped.")
+        main_df, embedding_map, label_map = encoding_step(df=df,
+                                                          spending_report=False,
+                                                          include_lag_features=False)
 
 
         X_train, X_train_scaled, X_train_minmax, X_test, X_test_scaled, \
@@ -244,12 +254,11 @@ def db_filler(section, plots=False, spending_report=False, include_lag_features=
         # array object
         y_pred = clf_object.predict(Xt_array)
 
-        # adding y_pred directly works fine
         merchants = []
-        # resulting variable is an array!
+        # label_map ('merchant' : '1')
         for i in y_pred:
-            for val, enc in embedding_map.items():
-                if enc in predictions:
+            for val, enc in label_map.items():
+                if enc in y_pred:
                     merchants.append(val)
                 else:
                     merchants.append("unseen in training")
@@ -309,10 +318,10 @@ def db_filler(section, plots=False, spending_report=False, include_lag_features=
         print("=========================")
 
 
-    return 'filling completed'
+    return 'completed'
 #%%
+# df_ext, embedding_map_ext, label_map_ext = encoding_step(df=df,
+#                                                   spending_report=False,
+#                                                   include_lag_features=False)
 db_filler(section=0,
-          plots=False,
-          spending_report=False,
-          include_lag_features=False,
           store_model=False)
